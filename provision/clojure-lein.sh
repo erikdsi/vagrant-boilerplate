bff9dba02262e44804234413fb7c86233dc6356c
Erik da Silva Ikuta <erikdsi.ctba@gmail.com>
2014-04-11 08:08:09 -0300

    added comment about lein

--- provision/clojure-lein.sh
+++ provision/clojure-lein.sh
@@ -3,3 +3,4 @@
 sudo apt-get install openjdk-7-jre
 mkdir $HOME/bin && curl https://raw.github.com/technomancy/leiningen/stable/bin/lein > $HOME/bin/lein
 chmod a+x $HOME/bin/lein
+# You probably need to relog/reopen the shell to use lein
